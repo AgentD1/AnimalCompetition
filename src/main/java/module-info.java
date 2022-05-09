e527b412f6e9c6984d6c4d586531d7f3a4334ab5
AgentD1 <jacobandian3@gmail.com>
2022-05-09 09:35:40 -0400

    Added fighting to the text area

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,7 +6,14 @@ module AnimalCompetition {
 	requires org.jetbrains.annotations;
 	
 	exports tech.jaboc.animalcompetition.animal;
+	opens tech.jaboc.animalcompetition.animal;
+	exports tech.jaboc.animalcompetition.animal.json;
+	opens tech.jaboc.animalcompetition.animal.json;
 	exports tech.jaboc.animalcompetition.environment;
+	opens tech.jaboc.animalcompetition.environment;
+	exports tech.jaboc.animalcompetition.contest;
+	opens tech.jaboc.animalcompetition.contest;
 	exports tech.jaboc.animalcompetition;
+	opens tech.jaboc.animalcompetition;
 	exports tech.jaboc.animalcompetition.gui to javafx.graphics;
 }
